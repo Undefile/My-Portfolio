021da2cdf4c8c5e95ca9715859bc90b8d0a69635
Your NameUndefile <kushalneupane842@gmail.com>
2024-12-30 20:34:22 +0545

    my portfolio using react and vite

---
+++ src/components/Experience.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div id="experience" className="flex min-h-screen w-full flex-col items-center justify-center gap-20 p-16 md:p-20    lg:px-36 lg:py-16">
+      <h1 className="text-center text-5xl font-light">Experience</h1>
+
+      <div className="flex flex-wrap gap-0 md:flex-nowrap md:gap-12">
+        <div className="flex-1">
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 1</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 2</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 3</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 4</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 5</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+
+          <div className="relative space-y-1 border-l-2 p-6 text-left">
+            <h3 className="text-xl font-semibold md:text-2xl">Experience 6</h3>
+            <p className="font-light text-lg text-gray-600">Job Title</p>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, odit!
+            </p>
+            <span className="absolute -left-[11px] top-10 h-5 w-5 rounded-full bg-teal-600"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div id='projects' className='min-h-screen w-full flex flex-col items-center justify-center gap-10 p-10 md:p-16 \
+     xl:px-16'>
+      <h1 className='text-center text-5xl font-light'>Projects</h1>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols:3 gap-5'>
+
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg '>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg'>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg '>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg'>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg '>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+        <div className='text-left border-2 space-y-2 px-8 hover:scale-105 transition-all duration-200 cursor-pointer py-10 border-teal-500 rounded-lg hover:bg-teal-50 hover:shadow-lg'>
+          <h1 className='text-3xl font-semibold'>Project 1</h1>
+          <h3 className='text-xl text-gray-700 '>Tech Stack</h3>
+          <p className='text-sm  text-gray-600 w-15'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate facere possimus! Architecto, ipsa. Officiis, quia? </p>
+        </div>
+
+
+      </div>
+    </div>
+    
+  )
+}
+
+export default Projects
